d1f11334f33d9add0eb54704c8cbfcd448c5207e
Astonished Carrot <56830886+WangJiayi2289@users.noreply.github.com>
2020-03-06 19:57:03 +0800

    1.0.2

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -2,32 +2,53 @@ Page({
   data: {
     weeklyMovieList: [
       {
-        name: "泰坦尼克号",
-        comment: "失去的才是永恒的",
-        imagePath: "/images/titanic.jpg",
-        isHighlyRecommended: false,
-        id: 1292722
-      },
-      {
-        name: "这个杀手不太冷",
-        comment: "小萝莉与怪蜀黍纯真灿烂的爱情故事",
-        imagePath: "/images/leon.jpg",
-        isHighlyRecommended: false,
-        id: 1295644
-      },
-      {
-        name: "教父",
-        comment: "最精彩的剧本，最真实的黑帮电影。",
-        imagePath: "/images/jf.jpg",
+        name: "天空之城",
+        comment: "飞行岛的神秘冒险。",
+        imagePath: "/images/tkzc.jpg",
         isHighlyRecommended: true,
-        id: 1291841
-      },
+        id: 1291583
+      }, 
       {
         name: "千与千寻",
         comment: "神隐少女的的神界冒险之旅。",
         imagePath: "/images/qx.jpg",
         isHighlyRecommended: true,
         id: 1291561
+      }, 
+      {
+        name: "龙猫",
+        comment: "乡间居住的温暖故事。",
+        imagePath: "/images/lm.jpg",
+        isHighlyRecommended: true,
+        id: 1291560
+      }, 
+      {
+        name: "魔女宅急便",
+        comment: "少女的修行往事。",
+        imagePath: "/images/mvzjb.jpg",
+        isHighlyRecommended: true,
+        id: 1307811
+      }, 
+      {
+        name: "哈尔的移动城堡",
+        comment: "魔法世界的奇妙历程。",
+        imagePath: "/images/hedydcb.jpg",
+        isHighlyRecommended: true,
+        id: 1308807
+      }, 
+      {
+        name: "起风了",
+        comment: "天才设计师的飞行梦。",
+        imagePath: "/images/qfl.jpg",
+        isHighlyRecommended: false,
+        id: 6791750
+      }, 
+      {
+        name: "悬崖上的金鱼公主",
+        comment: "金鱼公主的蜕变旅程。",
+        imagePath: "/images/xysdjygz.jpg",
+        isHighlyRecommended: true,
+        id: 1959877
       }, 
     ],
     count: 0,
